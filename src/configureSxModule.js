0f365f185a2eb7ba8dadfbe3f5805341834d64bf
eva <eva.de.haas@liones.nl>
2019-06-28 10:47:31 +0200

    Upgrade to SDK 7.7

--- src/configureSxModule.js
+++ src/configureSxModule.js
@@ -1,70 +1,59 @@
-define([
-	'fontoxml-families/configureAsFrame',
-	'fontoxml-families/configureAsSheetFrame',
-	'fontoxml-families/configureAsStructure',
-	'fontoxml-families/configureAsTitleFrame',
-	'fontoxml-families/configureContextualOperations',
-	'fontoxml-families/createMarkupLabelWidget',
-	'fontoxml-families/createRelatedNodesQueryWidget',
-	'fontoxml-localization/t'
-], function (
-	configureAsFrame,
-	configureAsSheetFrame,
-	configureAsStructure,
-	configureAsTitleFrame,
-	configureContextualOperations,
-	createMarkupLabelWidget,
-	createRelatedNodesQueryWidget,
-	t
-) {
-	'use strict';
+import configureAsFrame from 'fontoxml-families/src/configureAsFrame.js';
+import configureAsSheetFrame from 'fontoxml-families/src/configureAsSheetFrame.js';
+import configureAsStructure from 'fontoxml-families/src/configureAsStructure.js';
+import configureAsTitleFrame from 'fontoxml-families/src/configureAsTitleFrame.js';
+import configureContextualOperations from 'fontoxml-families/src/configureContextualOperations.js';
+import createMarkupLabelWidget from 'fontoxml-families/src/createMarkupLabelWidget.js';
+import createRelatedNodesQueryWidget from 'fontoxml-families/src/createRelatedNodesQueryWidget.js';
+import t from 'fontoxml-localization/src/t.js';
 
-	return function configureSxModule (sxModule) {
-		// learningAssessment
-		//     A Learning Assessment presents questions or interactions that measure progress, encourage
-		//     recollection, and stimulate reinforcement of the learning content, and can be presented before the
-		//     content as a pre-assessment or as a post-assessment test. The interactions use a sub-set of the
-		//     Question-Test Interoperability (QTI) specification, implemented as a DITA domain specialization.
-		configureAsSheetFrame(sxModule, 'self::learningAssessment', t('learning assessment'), {
+export default function configureSxModule(sxModule) {
+	// learningAssessment
+	//     A Learning Assessment presents questions or interactions that measure progress, encourage
+	//     recollection, and stimulate reinforcement of the learning content, and can be presented before the
+	//     content as a pre-assessment or as a post-assessment test. The interactions use a sub-set of the
+	//     Question-Test Interoperability (QTI) specification, implemented as a DITA domain specialization.
+	configureAsSheetFrame(sxModule, 'self::learningAssessment', t('learning assessment'), {
+		defaultTextContainer: 'learningAssessmentbody',
+		titleQuery:
+			'./title//text()[not(ancestor::*[name() = ("sort-at", "draft-comment", "foreign", "unknown", "required-cleanup", "image")])]/string() => string-join()',
+		blockFooter: [
+			createRelatedNodesQueryWidget('./related-links'),
+			createRelatedNodesQueryWidget(
+				'descendant::fn[not(@conref) and fonto:in-inline-layout(.)]'
+			)
+		],
+		blockHeaderLeft: [createMarkupLabelWidget()]
+	});
+
+	// learningAssessment nested in topic
+	configureAsFrame(
+		sxModule,
+		'self::learningAssessment and ancestor::*[fonto:dita-class(., "topic/topic")]',
+		undefined,
+		{
 			defaultTextContainer: 'learningAssessmentbody',
-			titleQuery: './title//text()[not(ancestor::*[name() = ("sort-at", "draft-comment", "foreign", "unknown", "required-cleanup", "image")])]/string() => string-join()',
-			blockFooter: [
-				createRelatedNodesQueryWidget('./related-links'),
-				createRelatedNodesQueryWidget('descendant::fn[not(@conref) and fonto:in-inline-layout(.)]')
-			],
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			]
-		});
+			blockFooter: [createRelatedNodesQueryWidget('./related-links')],
+			blockHeaderLeft: [createMarkupLabelWidget()]
+		}
+	);
 
-		// learningAssessment nested in topic
-		configureAsFrame(sxModule, 'self::learningAssessment and ancestor::*[fonto:dita-class(., "topic/topic")]', undefined, {
-			defaultTextContainer: 'learningAssessmentbody',
-			blockFooter: [
-				createRelatedNodesQueryWidget('./related-links')
-			],
-			blockHeaderLeft: [
-				createMarkupLabelWidget()
-			]
-		});
+	// title in learningAssessment
+	configureAsTitleFrame(sxModule, 'self::title[parent::learningAssessment]', undefined, {
+		fontVariation: 'document-title'
+	});
 
-		// title in learningAssessment
-		configureAsTitleFrame(sxModule, 'self::title[parent::learningAssessment]', undefined, {
-			fontVariation: 'document-title'
-		});
+	// learningAssessmentbody
+	//     The <learningAssessmentbody> element is the main body-level element in a learningAssessment topic.
+	configureAsStructure(sxModule, 'self::learningAssessmentbody', t('body'), {
+		defaultTextContainer: 'section',
+		ignoredForNavigationNextToSelector: 'false()',
+		isRemovableIfEmpty: false
+	});
 
-		// learningAssessmentbody
-		//     The <learningAssessmentbody> element is the main body-level element in a learningAssessment topic.
-		configureAsStructure(sxModule, 'self::learningAssessmentbody', t('body'), {
-			defaultTextContainer: 'section',
-			ignoredForNavigationNextToSelector: 'false()',
-			isRemovableIfEmpty: false
-		});
-
-		// section in learningAssessmentbody
-		configureContextualOperations(sxModule, 'self::section[parent::learningAssessmentbody]', [
-			{ name: ':section-insert-title' },
-			{ name: ':contextual-delete-section' }
-		]);
-	};
-});
+	// section in learningAssessmentbody
+	configureContextualOperations(sxModule, 'self::section[parent::learningAssessmentbody]', [
+		{ name: ':section-insert-title' },
+		{ name: ':contextual-delete-section' }
+	]);
+}
